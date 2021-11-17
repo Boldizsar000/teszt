fee7475b3ad9ae24cc0afb295b4ed0a9072cd782
Boldizsar000 <79318819+Boldizsar000@users.noreply.github.com>
2021-11-17 10:53:58 +0100

    Add files via upload

---
+++ nevek.py
@@ -0,0 +1,12 @@
+print
+nevek = ["Tibor","Andrea","Amira"]
+print ('Kérek egy nevet!')
+nev = input()
+for a in nevek:
+    if a == nev:
+        print("Van ilyen név")
+import random
+ÁTLAG = 15
+szam = random.randint(10,30)
+if szam == ÁTLAG:
+    print ("Átlaga")
